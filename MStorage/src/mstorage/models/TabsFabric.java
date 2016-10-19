525ff2b1d79dfa17bc29212e9f3b159dcd856983
naveter <naveter@eb688aa3-2587-47c8-b16e-59fa4ad59537>
2016-10-19 17:11:04 +0000

    1 vol.3

--- MStorage/src/mstorage/models/TabsFabric.java
+++ MStorage/src/mstorage/models/TabsFabric.java
@@ -42,6 +42,13 @@ public class TabsFabric {
 		PanelTemplate.JPanelDocumentPictures = new javax.swing.JPanel();
 		PanelTemplate.TextAreaDocument = new javax.swing.JTextArea();
 		PanelTemplate.TextAreaDocument.addFocusListener(PanelTemplate);
+        
+        // Elements search in file
+        PanelTemplate.jPanelSearchInFile = new javax.swing.JPanel();
+        PanelTemplate.jButtonCloseSearchInFilePanel = new javax.swing.JButton();
+        PanelTemplate.jTextFieldSearchInFile = new javax.swing.JTextField();
+        PanelTemplate.jButtonSearchInFile = new javax.swing.JButton();
+        PanelTemplate.jCheckBoxFindInFileUseCase = new javax.swing.JCheckBox();
 
 		PanelTemplate.setPreferredSize(new java.awt.Dimension(300, 300));
 
@@ -88,15 +95,68 @@ public class TabsFabric {
         
 		PanelTemplate.TextAreaDocument.setWrapStyleWord(true);
 		PanelTemplate.ScrollPaneDocumentText.setViewportView(PanelTemplate.TextAreaDocument);
+        
+        
+        // Elements search in file
+        PanelTemplate.jPanelSearchInFile.setPreferredSize(new java.awt.Dimension(0, 30));
 
+        PanelTemplate.jButtonCloseSearchInFilePanel.setIcon(
+            new javax.swing.ImageIcon(
+                MainForm.getInstance().getClass().getResource("/images/cross.16x16.png")
+            )
+        ); // NOI18N
+        PanelTemplate.jButtonCloseSearchInFilePanel.setContentAreaFilled(false);
+        PanelTemplate.jButtonCloseSearchInFilePanel.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                PanelTemplate.jPanelSearchInFile.setVisible(false);
+                MainForm.getInstance().getjButtonSearchInFile().setSelected(false);
+            }
+        });
+
+        PanelTemplate.jButtonSearchInFile.setText("Find");
+        PanelTemplate.jButtonSearchInFile.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                System.out.println("jButtonSearchInFile");
+            }
+        });
+
+        PanelTemplate.jCheckBoxFindInFileUseCase.setText("Use case");
+        
+        javax.swing.GroupLayout jPanelSearchInFileLayout = new javax.swing.GroupLayout(PanelTemplate.jPanelSearchInFile);
+        PanelTemplate.jPanelSearchInFile.setLayout(jPanelSearchInFileLayout);
+        jPanelSearchInFileLayout.setHorizontalGroup(
+            jPanelSearchInFileLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelSearchInFileLayout.createSequentialGroup()
+                .addGap(6, 6, 6)
+                .addComponent(PanelTemplate.jTextFieldSearchInFile, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(PanelTemplate.jButtonSearchInFile)
+                .addGap(18, 18, 18)
+                .addComponent(PanelTemplate.jCheckBoxFindInFileUseCase)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 127, Short.MAX_VALUE)
+                .addComponent(PanelTemplate.jButtonCloseSearchInFilePanel, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        jPanelSearchInFileLayout.setVerticalGroup(
+            jPanelSearchInFileLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(PanelTemplate.jButtonCloseSearchInFilePanel)
+            .addGroup(jPanelSearchInFileLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addComponent(PanelTemplate.jTextFieldSearchInFile, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(PanelTemplate.jButtonSearchInFile)
+                .addComponent(PanelTemplate.jCheckBoxFindInFileUseCase))
+        );
+        PanelTemplate.jPanelSearchInFile.setVisible(false);
+
+        
 		javax.swing.GroupLayout PanelTemplateLayout = new javax.swing.GroupLayout(PanelTemplate);
 		PanelTemplate.setLayout(PanelTemplateLayout);
 		PanelTemplateLayout.setHorizontalGroup(PanelTemplateLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
 				.addComponent(PanelTemplate.ScrollPaneDocumentText, javax.swing.GroupLayout.DEFAULT_SIZE, 471, Short.MAX_VALUE)
 				.addComponent(PanelTemplate.JPanelDocumentPictures)
+                .addComponent(PanelTemplate.jPanelSearchInFile, javax.swing.GroupLayout.DEFAULT_SIZE, 467, Short.MAX_VALUE)
 		);
 		PanelTemplateLayout.setVerticalGroup(PanelTemplateLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
 				.addGroup(PanelTemplateLayout.createSequentialGroup()
+                        .addComponent(PanelTemplate.jPanelSearchInFile, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
 						.addComponent(PanelTemplate.ScrollPaneDocumentText, javax.swing.GroupLayout.DEFAULT_SIZE, 386, Short.MAX_VALUE)
 						.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
 						.addComponent(PanelTemplate.JPanelDocumentPictures, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -109,5 +169,7 @@ public class TabsFabric {
 
 		return PanelTemplate;
 	}
+    
+
 
 }
